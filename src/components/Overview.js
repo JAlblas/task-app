89c5343c1df704b3bcd7b1ec098220a218f2dcfd
Jasper Alblas <just_jasper@hotmail.com>
2021-02-01 15:16:27 +0100

    cleanup

---
+++ src/components/Overview.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Overview extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div className="Overview">
+            Overview
+            </div>
+        )
+    }
+}
+
+export default Overview
